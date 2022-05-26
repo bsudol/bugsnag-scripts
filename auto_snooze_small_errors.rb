b38c508ad5af9f8a98fd0f7e693168115a169579
bsudol <barbara.sudol@SB-MA-0XKMD6N.myfiosgateway.com>
2022-05-26 16:30:20 -0400

    added date calculations and other logic

--- auto_snooze_small_errors.rb
+++ auto_snooze_small_errors.rb
@@ -3,51 +3,69 @@
 
 require 'httparty'
 require 'json'
+require 'date'
 
-@auth_token = "YOUR_API_KEY"
+@auth_token = "" #Your API Token here
 @minimum_events = 8 #Snooze all errors with event count less than this number
-@project_id = '61fa9fef78e179000f56854c'
+@number_of_days = 7 #Pull and modify errors occuring in the last [number_of_days] days (coincides with frequency of script)
+@project_name = 'example-project' #Set to your project name
 
-# @get_organizations_url= "https://api.bugsnag.com/user/organizations?admin=&per_page=10"
+#######################
+@get_organizations_url= "https://api.bugsnag.com/user/organizations?admin=&per_page=10"
 
-# org_response = HTTParty.get(@get_organizations_url,
-# :headers => { 'Authorization' => "token #{@auth_token}",'Content-Type' => 'application/json',  }
-# )
-
-# organization_id = org_response[0]['id']
-
-# @get_projs_url = "https://api.bugsnag.com/organizations/#{organization_id}/projects?sort=created_at&direction=desc&per_page=30"
-# projects_response = HTTParty.get(@get_projs_url,
-# :headers => { 'Authorization' => "token #{@auth_token}",'Content-Type' => 'application/json',  }
-# )
-# puts projects_response
-
-
-#TODO: Define a timestamp for x days ago (frequency of script running)
-@get_errors_url = "https://api.bugsnag.com/projects/#{@project_id}/errors?sort=last_seen&direction=desc&per_page=50&filters[error.status][][type]=eq&filters[error.status][][value]=open&filters[event.since][][type]=eq&filters[event.since][][value]=2022-05-20T00:00:00Z"
-
-errors_response = HTTParty.get(@get_errors_url,
-:headers => { 'Authorization' => "token #{@auth_token}",'Content-Type' => 'application/json' }
+org_response = HTTParty.get(@get_organizations_url,
+:headers => { 'Authorization' => "token #{@auth_token}",'Content-Type' => 'application/json',  }
 )
 
-errors_to_update = Array.new
+organization_id = org_response[0]['id']
 
-errors_response.each do |error|
-	if error['events'] <= @minimum_events
-		errors_to_update << error['id']
-	end	
+@get_projs_url = "https://api.bugsnag.com/organizations/#{organization_id}/projects?sort=created_at&direction=desc&per_page=30"
+projects_response = HTTParty.get(@get_projs_url,
+:headers => { 'Authorization' => "token #{@auth_token}",'Content-Type' => 'application/json',  }
+)
+
+projects_response.each do |project|
+	if project['name'] == @project_name
+		@project_id = project['id']
+		break
+	end
 end
 
-@update_errors_url = "https://api.bugsnag.com/projects/#{@project_id}/errors"
+if @project_id
 
-bulk_snooze_response = HTTParty.patch(@update_errors_url,
-:headers => { 'Authorization' => "token #{@auth_token}",'Content-Type' => 'application/json' },
-:query => {'error_ids' => errors_to_update},
-:body => { :operation => "snooze",
-		   :reopen_rules => {
-		   		reopen_if: "n_additional_occurrences",
-		   		additional_occurrences: @minimum_events
-		   	}
-		  }.to_json )
+	earliest_date = (DateTime.now - @number_of_days).strftime('%Y-%m-%dT%H:%M:%SZ')
 
-puts bulk_snooze_response
+	@get_errors_url = "https://api.bugsnag.com/projects/#{@project_id}/errors?sort=last_seen&direction=desc&per_page=50&filters[error.status][][type]=eq&filters[error.status][][value]=open&filters[event.since][][type]=eq&filters[event.since][][value]=#{earliest_date}"
+
+	errors_response = HTTParty.get(@get_errors_url,
+	:headers => { 'Authorization' => "token #{@auth_token}",'Content-Type' => 'application/json' }
+	)
+
+	errors_to_update = Array.new
+
+	errors_response.each do |error|
+		if error['events'] <= @minimum_events
+			errors_to_update << error['id']
+		end	
+	end
+
+	if errors_response.length() > 0
+
+		@update_errors_url = "https://api.bugsnag.com/projects/#{@project_id}/errors"
+
+		bulk_snooze_response = HTTParty.patch(@update_errors_url,
+		:headers => { 'Authorization' => "token #{@auth_token}",'Content-Type' => 'application/json' },
+		:query => {'error_ids' => errors_to_update},
+		:body => { :operation => "snooze",
+				   :reopen_rules => {
+				   		reopen_if: "n_additional_occurrences",
+				   		additional_occurrences: @minimum_events
+				   	}
+				  }.to_json )
+
+		puts bulk_snooze_response
+	end
+
+else
+	puts "No project found with name #{@project_name}"
+end
